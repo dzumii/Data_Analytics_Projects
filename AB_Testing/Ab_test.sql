3a31f6359af6916ef092d416656cdcdfb23d05b7
ADEYEMI Jumoke Olantun <adeyemijumokeolantun@gmail.com>
2024-10-25 18:25:49 +0100

    Update Ab_test.sql

--- AB_Testing/Ab_test.sql
+++ AB_Testing/Ab_test.sql
@@ -1,14 +1,25 @@
+WITH mean_table AS (
 SELECT
     location_id,
     promotion,
-    AVG(sales_in_thousands) AS avg_sales,
+    SUM(sales_in_thousands) AS sum_sales,
     COUNT(week) AS weeks_count
 FROM
     `tc-da-1.turing_data_analytics.wa_marketing_campaign`
-
 
 GROUP BY
     location_id,
     promotion
 ORDER BY
-    promotion, location_id;
+    promotion, location_id
+)
+
+SELECT 
+promotion,
+ROUND(AVG(sum_sales) ,2) as avg_sales,
+ROUND(STDDEV(sum_sales),2) as stddev_sales,
+COUNT (location_id) as no_of_locations
+FROM mean_table
+GROUP BY
+promotion;
+
